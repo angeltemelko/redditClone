aff6f69362ba5f253f0ad2f6740552fb3e2b68a1
Angel Temelko <angel_aleksandar@hotmail.com>
2020-08-14 16:40:19 +0200

    Created entities for database

---
+++ src/main/java/com/angel/demo/model/Comment.java
@@ -0,0 +1,18 @@
+package com.angel.demo.model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String body;
+
+}
