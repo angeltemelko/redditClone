3bd93026174df14dbb436d92e31360256ac44f89
Angel Temelko <angel_aleksandar@hotmail.com>
2020-08-15 16:16:36 +0200

    Added Repositories

---
+++ src/main/java/Repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package Repository;
+
+import com.angel.demo.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote,Long> {
+}
