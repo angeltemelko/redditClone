9ae542039074911d3a97af6c13a38b93c992c648
Angel Temelko <angel_aleksandar@hotmail.com>
2020-08-15 19:46:14 +0200

    Auditable class added

--- src/main/java/com/angel/demo/model/Comment.java
+++ src/main/java/com/angel/demo/model/Comment.java
@@ -4,15 +4,19 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Comment {
+public class Comment extends Auditable{
 
     @Id
     @GeneratedValue
     private long id;
     private String body;
+
+    @ManyToOne
+    Link link;
 
 }
--- src/main/java/com/angel/demo/model/Link.java
+++ src/main/java/com/angel/demo/model/Link.java
@@ -4,15 +4,21 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Link {
+public class Link extends Auditable{
 
     @Id
     @GeneratedValue
     private long id;
     private String title;
     private String url;
+
+    @OneToMany(mappedBy = "link")
+    List<Comment> comments = new ArrayList<>();
 }
