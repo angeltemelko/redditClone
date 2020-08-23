4ce9e3ceb565bcc7a2ef1e74d2019379c7e3148c
Angel Temelko <angel_aleksandar@hotmail.com>
2020-08-23 17:56:00 +0200

    Created the link RestController

--- src/main/java/com/angel/demo/Controller/HomeController.java
+++ src/main/java/com/angel/demo/Controller/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+/*
     @GetMapping("/")
     public String home(){
         return "hello";
-    }
+    }*/
 }
