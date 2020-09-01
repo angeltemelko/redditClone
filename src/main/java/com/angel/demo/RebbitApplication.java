bc37e70f53f04454949b9816e5a15e1161f2fdad
Angel Temelko <angel_aleksandar@hotmail.com>
2020-09-01 19:26:34 +0200

    Created several pages and updated the controller

--- src/main/java/com/angel/demo/RebbitApplication.java
+++ src/main/java/com/angel/demo/RebbitApplication.java
@@ -1,10 +1,8 @@
 package com.angel.demo;
 
-import com.angel.demo.Repository.CommentRepository;
-import com.angel.demo.Repository.LinkRepository;
-import com.angel.demo.model.Comment;
-import com.angel.demo.model.Link;
-import org.springframework.boot.CommandLineRunner;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,20 +12,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class RebbitApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(RebbitApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(RebbitApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-        return args -> {
-            Link link = new Link("My spring boot","This is the second string");
-            linkRepository.save(link);
-            Comment comment = new Comment("This is a comment",link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-
-        };
+    public PrettyTime prettyTime(){
+        return new PrettyTime();
     }
+
 
 }
--- src/main/java/com/angel/demo/model/Link.java
+++ src/main/java/com/angel/demo/model/Link.java
@@ -1,14 +1,23 @@
 package com.angel.demo.model;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
+import com.angel.demo.service.BeanUtil;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -16,12 +25,16 @@ import java.util.List;
 @NoArgsConstructor
 public class Link extends Auditable{
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private long id;
+
     @NonNull
+    @NotEmpty( message = "please enter a title")
     private String title;
+
     @NonNull
+    @NotEmpty( message = "please enter a url")
+    @URL( message = "please enter a valid url")
     private String url;
 
     @OneToMany(mappedBy = "link")
@@ -35,6 +48,21 @@ public class Link extends Auditable{
 
     public void addComment(Comment comment){
         comments.add(comment);
+    }
+
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 
 }
